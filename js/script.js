ccedd0a7574ed16d4e12be6b013abc2bf6f1f1b2
James Almeida <james@peerdustry.com>
2019-06-11 20:42:49 -0300

    fix menu

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ const menu = document.querySelector('[menu-drop]');
 const hamburger = document.querySelector('[hamburger]');
 
 const dropMenu = () => {
-  console.log('cheguei')
-  menu.classList.remove('.toggle-menu');
+  menu.classList.toggle('toggle-menu');
 }
 
 hamburger.onclick = dropMenu;
