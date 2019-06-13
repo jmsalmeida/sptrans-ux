b5efa573a62008492ff5ab8f5b6a09a374d3aaf0
James Almeida <james@peerdustry.com>
2019-06-13 13:33:38 -0300

    message help

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 const menu = document.querySelector('[menu-drop]');
 const hamburger = document.querySelector('[hamburger]');
+const ballon = document.querySelector('.chat-balloon');
+
+function hideBallon() {
+  ballon.style.display = 'none';
+}
+
+setTimeout(hideBallon, 5000);
 
 const dropMenu = () => {
   menu.classList.toggle('toggle-menu');
